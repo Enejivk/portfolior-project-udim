06f67b12a47034b92bbe448bebf95d84891f11cf
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2024-06-16 16:00:38 +0100

    added the models

---
+++ api/app.py
@@ -0,0 +1,8 @@
+from api.v1 import app
+from os import getenv
+
+
+if __name__ == '__main__':
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(getenv('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port, threaded=True)
---
+++ api/v1/__init__.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+from flask import Flask, make_response, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+
+
+# Environment Variables
+HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
+HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
+HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
+HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
+HBNB_ENV = getenv('HBNB_ENV')
+DEBUG = True
+uri = 'mysql+mysqldb://{}:{}@{}/{}'.format(HBNB_MYSQL_USER,
+                                           HBNB_MYSQL_PWD,
+                                           HBNB_MYSQL_HOST,
+                                           HBNB_MYSQL_DB)
+
+# Flask Application Setup
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
+db = SQLAlchemy(app)
+
+from api.v1.models import User, Group, Member, Message
+@app.errorhandler(404)
+def not_found(error):
+    """
+    Handle 404 errors by returning a JSON response indicating resource
+    not found.
+    """
+    return make_response(jsonify({"error": "Not found"}), 404)
