f17841a914a3e9da1127fd0990fcb52f57a2c3ca
enejivk <enejivic@gmail.com>
2024-06-17 10:03:45 +0100

    [footer, feature] increasing padding

--- udim_frontend/src/landingPage/footer/Footer.jsx
+++ udim_frontend/src/landingPage/footer/Footer.jsx
@@ -3,34 +3,34 @@ import logoBlack from '../../assets/logoBlack.png'
 import './footer.css';
 
 const Footer = () => (
-  <section className="gpt3__footer section__padding">
-    <div className="gpt3__footer-heading">
+  <section className="footer section__padding">
+    <div className="footer-heading">
       <h1 className="gradient__text">Do you want to step in to the future before others</h1>
     </div>
 
-    <div className="gpt3__footer-btn">
-      <p>Request Early Access</p>
+    <div className="footer-btn">
+      <p><a href="">Get started today</a></p>
     </div>
 
-    <div className="gpt3__footer-links">
-      <div className="gpt3__footer-links_logo">
+    <div className="footer-links">
+      <div className="footer-links_logo">
         <img src={logoBlack} alt="gpt3_logo" />
         <p>Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved</p>
       </div>
-      <div className="gpt3__footer-links_div">
+      <div className="footer-links_div">
         <h4>Links</h4>
         <p>Overons</p>
         <p>Social Media</p>
         <p>Counters</p>
         <p>Contact</p>
       </div>
-      <div className="gpt3__footer-links_div">
+      <div className="footer-links_div">
         <h4>Company</h4>
         <p>Terms & Conditions </p>
         <p>Privacy Policy</p>
         <p>Contact</p>
       </div>
-      <div className="gpt3__footer-links_div">
+      <div className="footer-links_div">
         <h4>Get in touch</h4>
         <p>Crechterwoord K12 182 DK Alknjkcb</p>
         <p>085-132567</p>
@@ -38,7 +38,7 @@ const Footer = () => (
       </div>
     </div>
 
-    <div className="gpt3__footer-copyright">
+    <div className="footer-copyright">
       <p>@2041 Udim. All rights reserved.</p>
     </div>
   </section>
